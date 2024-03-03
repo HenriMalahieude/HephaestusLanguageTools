fa2b64253151c0087ea996ded917bb18d1a0cde9
HenriMalahieude <henrimalahieude@gmail.com>
2024-03-02 23:38:24 -0800

    developing an easier way for me to write these test files

---
+++ S1_Lexic/test/test_helper.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+//Test Regex Section
+#include "../private/regex.h"
+
+#ifndef VERBOSE
+
+#define TEST_REGEX_TRUE(name, num, strvar, strconst, rgx) ({\
+		strcpy(strvar, strconst);\
+		if (rgx->match_function(strvar, rgx->attached_data)) {\
+			printf("[X] Regex %s Test %d Fail w/ %s\n", name, num, strvar);\
+		}\
+		})
+
+#define TEST_REGEX_FALSE(name, num, strvar, strconst, rgx) ({\
+		strcpy(strvar, strconst);\
+		if (!rgx->match_function(strvar, rgx->attached_data)) {\
+			printf("[X] Regex %s Test %d Fail w/ %s\n", name, num, strvar);\
+		}\
+		})
+
+#endif
+
+//--------------------------------------------------------------------------------------------
+
+#ifdef VERBOSE
+
+#define TEST_REGEX_TRUE(name, num, strvar, strconst, rgx) ({\
+		printf("[?] Regex %s Test %d Result: ", name, num);\
+		strcpy(strvar, strconst);\
+		if (rgx->match_function(strvar, rgx->attached_data)) {\
+			printf("[X] w/ %s\n", strvar);\
+		} else {\
+			printf("[+]\n");\
+		}\
+		})
+
+#define TEST_REGEX_TRUE(name, num, strvar, strconst, rgx) ({\
+		printf("[?] Regex %s Test %d Result: ", name, num);\
+		strcpy(strvar, strconst);\
+		if (!rgx->match_function(strvar, rgx->attached_data)) {\
+			printf("[X] w/ %s\n", strvar);\
+		} else {\
+			printf("[+]\n");\
+		}\
+		})
+
+#endif
+
