f4e13f76c848b181909de8fac9abfce4bd562815
HenriMalahieude <henrimalahieude@gmail.com>
2024-03-07 18:17:17 -0800

    New group type of regex

--- S1_Lexic/private/definition.c
+++ S1_Lexic/private/definition.c
@@ -1,24 +1,6 @@
 #include <string.h>
 #include "regex.h"
 
-//TODO: Groups like (\.[0-9]+)?
-//TODO: Refactor Or Regexes to be more global
-
-void add_to_regex_sequence(struct regex ***sequence_ptr, int *count, struct regex *reg) {
-	*count += 1;
-	*sequence_ptr = realloc(*sequence_ptr, *count * sizeof(struct regex **));
-	if (*sequence_ptr == NULL) Regex_Error("Got realloc error in definition creation");
-
-	(*sequence_ptr)[*count-1] = reg;
-}
-
-void create_or_regex_and_add_to_sequence(struct regex ***sequence_ptr, int *count, struct regex *reg) {
-	struct regex *last = (*sequence_ptr)[*count-1];
-	struct regex *new_or = Regex_New_Or(last, reg); //NOTE: y'know, we could change the ordering to hopefully avoid a bunch of func calls? meh...?
-	
-	(*sequence_ptr)[*count-1] = reg;
-}
-
 struct regex** Regex_New_Definition(char *definition, int *count) {
 	struct regex **sequence = NULL;
 	*count = 0;
@@ -49,103 +31,29 @@ struct regex** Regex_New_Definition(char *definition, int *count) {
 		}
 	}
 
-	char *debug_copy = calloc((end_pos-consume_pos)+1, sizeof(char)); strncopy(debug_copy, definition + consume_pos, (end_pos-consume_pos));
-	printf("\tRegex Debug: trimmed definition is '%s'\n", debug_copy);
+	char *trimmed_internals = calloc((end_pos-consume_pos)+1, sizeof(char)); 
+	strncopy(trimmed_internals, definition + consume_pos, (end_pos-consume_pos));
 
-	enum regex_type mode = RT_DIRECT;//default interpretation
-	int or_level = 0;
-	char substr[100]; //If things really get long, this can be changed
-	for (size_t i = consume_pos; i <= end_pos; i++) {//NOTE: end_pos is an index, not length.
-		//detect empty space
-		formatted[0] = definition[i];
-		if (ss->match_function(formatted, ss->attached_data)) Regex_Warning("Provided regex definition has empty/whitespace within definition. Consider using '\\s' instead.");
+	printf("\tRegex Debug: trimmed definition is '%s'\n", trimmed_internals);
+	
+	struct regex *group = Regex_New_Group(trimmed_internals);
+	if (group->type != RT_GROUP) {
+		sequence = malloc(sizeof(struct regex *));
+		sequence[0] = group;
+		*count = 1;
+	} else {
+		*count = *(int*)((long long int *)group->attached_data)[0];
+		sequence = (struct regex **)((long long int *)group->attached_data)[1];
+		
+		//Precautions
+		group->type = RT_UNDEFINED;
+		group->match_function = NULL;
 
-		char cur = definition[i];
-		if (mode == RT_DIRECT) {
-			if (cur == ']') Regex_Error("Mismatched right bracket. Did you mean '\\]'?");
+		free(group->attached_data);
+		group->attached_data = NULL; //more precautions
 
-			if (cur == '\\') {
-				mode = RT_ESCAPED;
-			} else if (cur == '?' || cur == '*' || cur == '+') {
-				mode = RT_QUALIFIER;
-			} else if (cur == '[') {
-				mode = RT_BRACKETS;
-			} else if (cur == '|') {
-				or_level++;
-			} else if (i == end_pos){
-				mode = RT_UNDEFINED; //reached end of the line
-			}
-
-			if (consume_pos >= (i-1)) { //we consumed everything before it
-				if (mode == RT_QUALIFIER && *count == 0) Regex_Error("Qualifier must have preceding regex");
-				if (mode == RT_OR && *count == 0) Regex_Error("Or statements must have preceding regex");
-			} else if (mode != RT_DIRECT) {
-				int use = (mode == RT_UNDEFINED) ? i : i-1;
-				strncopy(substr, definition+consume_pos, use-consume_pos);
-				substr[use-consume_pos+1] = '\0';
-
-				struct regex *direct_reg = Regex_New_Direct(substr);
-				if (or_level <= 0) {
-					add_to_regex_sequence(&sequence, count, direct_reg);
-				} else {
-					create_or_regex_and_add_to_sequence(&sequence, count, direct_reg);
-					or_level--;
-				}
-
-				consume_pos = i+1;
-			}
-		} else if (mode == RT_ESCAPED) {
-			substr[0] = '\\';
-			substr[1] = definition[i];
-			substr[2] = '\0';
-			if (sequence[*count-1]->type != RT_OR) {
-				add_to_regex_sequence(&sequence, count, Regex_New_Escaped(substr));
-			} else {
-				create_or_regex_and_add_to_sequence(&sequence, count, Regex_New_Escaped(substr));
-				or_level--;
-			}
-
-			mode = RT_DIRECT;
-			consume_pos = i+1;
-		} else if (mode == RT_QUALIFIER) {
-			struct regex *last = sequence[*count-1];
-			if (last->type == RT_QUALIFIER) Regex_Error("Cannot have a qualifier after a qualifer.");
-
-			char qualifier = definition[i-1];
-			if (or_level <= 0) {
-				struct regex *qualified = Regex_New_Qualifier(last, qualifier);
-				sequence[*count-1] = qualified;
-			} else {
-				if (last->type != RT_OR) Regex_Error("How did we have an or level > 0 but no or?");
-
-				struct regex *last_or_item = ((struct regex **)last->attached_data)[1];
-				if (last_or_item->type == RT_QUALIFIER) Regex_Error("Cannot have a qualifier after a qualifier.");
-
-				struct regex *qualified = Regex_New_Qualifier(last_or_item, qualifier);
-				((struct regex **)last->attached_data)[1] = qualified;
-
-				or_level--;
-			}
-			
-			mode = RT_DIRECT;
-			consume_pos = i;
-			i--;//we haven't yet looked at this character, so go back!
-		} else if (mode == RT_BRACKETS) {
-			if (definition[i] == ']' && definition[i-1] != '\\') {
-				strncopy(substr, definition+consume_pos, (i-1)-consume_pos);
-				substr[(i-1)-consume_pos+1] = '\0';
-				if (or_level <= 0) {
-					add_to_regex_sequence(&sequence, count, Regex_New_Brackets(substr));
-				} else {
-					create_or_regex_and_add_to_sequence(&sequence, count, Regex_New_Brackets(substr));
-					or_level--;
-				}
-
-				consume_pos = i+1;
-			} else if (i == end_pos) {
-				Regex_Error("Mismatched right bracket. Reached end of definition without left bracket ']'");
-			}
-		}
+		free(group);
+		group = NULL:
 	}
 
 	return sequence;
--- S1_Lexic/private/regex.c
+++ S1_Lexic/private/regex.c
@@ -135,3 +135,163 @@ struct regex* Regex_New_Escaped(char *special) {
 	reg->match_function = Regex_Match_Escaped;
 	return reg;
 }
+
+//Helper for Regex_New_Group
+void add_to_grouping(struct regex ***grp, int *count, struct regex *reg) {
+	*count += 1;
+	*grp = realloc(*grp, *count * sizeof(struct regex *));
+	if (grp == NULL) Regex_Error("Realloc error in grouping!");
+
+	(*grp)[*count-1] = reg;
+}
+
+struct regex* Regex_New_Group(char *internals) {
+	if (strlen(internals) <= 0) Regex_Error("New Group. Internals were empty?");
+
+	int *count = calloc(1, sizeof(int));
+	struct regex **grouping = NULL;
+
+	struct regex *reg = malloc(sizeof(struct regex));
+	reg->type = RT_GROUP;
+
+	struct regex *ss = Regex_New_Escaped("\\s");
+
+	char substr[100];
+	char esc[2] = "a";
+	size_t consume_pos = 0;
+	enum regex_type mode = RT_DIRECT;
+	for (size_t i = 0; i < strlen(internals); i++) {
+		char cur = internals[i];
+
+		esc[0] = cur;
+		if (ss->match_function(esc, ss->attached_data)) Regex_Warning("Regex Definition contains whitespace. Consider using '\\s' instead.");
+
+		if (mode == RT_DIRECT) {
+			if (cur == ']') Regex_Error("Mismatched Right Bracket. Did you mean '\\]'?");
+			if (cur == ')') Regex_Error("Mismatched Right Parenthesis. Did you mean '\\)?");
+			
+			if (cur == '(') {
+				mode = RT_GROUP;
+			} else if (cur == '\\') {
+				mode = RT_ESCAPED;
+			} else if (cur == '?' || cur == '*' || cur == '+') { //NOTE: we need a better way to detect this
+				mode = RT_QUALIFIER;
+			} else if (cur == '[') {
+				mode = RT_BRACKETS;
+			} else if (cur == '|') {
+				mode == RT_OR;
+			} else if (i == (strlen(internals)-1)) {
+				mode = RT_UNDEFINED; //end of internals
+			}
+
+			if (consume_pos >= (i-1)) {
+				if (mode == RT_QUALIFIER && *count == 0) Regex_Error("Missing Regex for Qualifier");
+				if (mode == RT_OR && *count == 0) Regex_Error("Missing Regex for Or");
+				consume_pos++;
+			} else if (mode != RT_DIRECT) {
+				int j = (mode == RT_UNDEFINED) ? i : i - 1;
+				strncopy(substr, definition+consume_pos, j-consume_pos);
+				substr[j-consume_pos+1] = '\0';
+				
+				struct regex *direct = Regex_New_Direct(substr);
+				if (or_level <= 0) {
+					add_to_group(&grouping, count, direct);
+				} else {
+					add_or_to_group(&grouping, count, direct);
+					or_level--;
+				}
+
+				consume_pos = i+1;
+			}
+		} else if (mode == RT_GROUP) {
+			if (cur == ')' && internals[i-1] != '\\') {
+				strncopy(substr, internals+consume_pos, (i-1)-consume_pos);
+				substr[i-consume_pos] = '\0';
+
+				add_to_group(&grouping, count, Regex_New_Group(substr));
+
+				mode = RT_DIRECT;
+				consume_pos = i+1;
+			} else if (i == (strlen(internals)-1)) {
+				Regex_Error("Missing right parenthesis for group. Reached end of definition without right parenthesis ')'.");
+			}
+		} else if (mode == RT_ESCAPED) {
+			substr[0] = '\\'; substr[1] = cur; substr[2] = '\0';
+
+			add_to_group(&grouping, count, Regex_New_Escaped(substr));
+
+			mode = RT_DIRECT;
+			consume_pos = i+1;
+		} else if (mode == RT_QUALIFIER) {
+			struct regex *last = grouping[*count-1];
+			if (last->type == RT_QUALIFIER) Regex_Error("Cannot have a qualified qualifier.");
+
+			char qualifier = internals[i-1];
+			if (last->type == RT_OR) {
+				struct regex *or_item = ((struct regex **)last->attached_data)[1];
+				if (or_item->type == RT_QUALIFIER) Regex_Error("Cannot have a qualified qualifier.");
+				struct regex *qualified = Regex_New_Qualifier(or_item, qualifier);
+				((struct regex **)last->attached_data)[1] = qualified;
+			} else {
+				struct regex *qualified = Regex_New_Qualifier(last, qualifier);
+				grouping[*count-1] = qualified;
+			}
+
+			mode = RT_DIRECT;
+			consume_pos = i;
+			i--; //for loop gonna push it forward, this brings that back
+		} else if (mode == RT_BRACKETS) {
+			if (cur == ']' && internals[i-1] != '\\') {
+				strncopy(substr, definition+consume_pos, (i-1)-consume_pos);
+				substr[i-consume_pos] = '\0';
+
+				add_to_group(&grouping, count, Regex_New_Brackets(substr));
+
+				mode = RT_DIRECT;
+				consume_pos = i+1;
+			} else if (i == (strlen(internals)-1)) {
+				Regex_Error("Missing right bracket for bracket-group. Reached end of definition without right bracket ']'.");
+			}
+		} else if (mode == RT_OR) {
+			int len = strlen(internals) - consume_pos;
+			if (len <= 0) Regex_Error("Cannot Or with nothing. '|' at end of group/definition.");
+
+			strncopy(substr, internals+consume_pos, len);
+			substr[len+1] = '\0';
+
+			struct regex *all_prev = malloc(sizeof(struct regex));
+			all_prev->type = RT_GROUP;
+			all_prev->attached_data = malloc(2*sizeof(long long int));
+			all_prev->match_function = Regex_Match_Group;
+			
+			((long long int *)all_prev->attached_data)[0] = (long long int)count;
+			((long long int *)all_prev->attached_data)[1] = (long long int)grouping;
+
+			struct regex *all_aftr = Regex_New_Group(substr);
+
+			count = malloc(sizeof(int)); *count = 1;
+			grouping = malloc(sizeof(struct regex *)); grouping[0] = Regex_New_Or(all_prev, all_aftr);
+			
+			break; //We've consumed everything
+		}
+	}
+
+	Regex_Free(ss); ss = NULL; //Good memory management
+	
+	if (*count == 0) Regex_Error("New Group. Internal error, was not able to produce a single regex?");
+	
+	if (*count == 1) {
+		struct regex *only = grouping[0];
+		free(grouping);
+		free(count);
+		free(reg);
+		return only;
+	}
+	
+	reg->match_function = Regex_Match_Group;
+	reg->attached_data = malloc(2*sizeof(long long int));
+	((long long int *)reg->attached_data)[0] = (long long int)count;
+	((long long int *)reg->attached_data)[1] = (long long int)grouping;
+	
+	return reg;
+}
