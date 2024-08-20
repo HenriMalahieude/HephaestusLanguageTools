1fb38ad7b92c7dd9b007b6a886ba1bf98ec41874
HenriMalahieude <henrimalahieude@gmail.com>
2024-08-20 15:42:42 -0600

    Vocab Test had a bug + formatting for an all test later

--- Lexic/test/vocab.c
+++ Lexic/test/vocab.c
@@ -5,15 +5,17 @@
 #include "../private/lexic_internal.h"
 #include "../private/warn.h"
 
-void ReportResult(char *msg, bool result) {
+bool ReportResult(char *msg, bool result) {
 #ifdef VERBOSE
 	printf("[?] Vocab Test (%s): %d\n", msg, result);
 #else
 	if (!result) printf("[X] Vocab Test (%s) failed\n", msg);
 #endif
+
+	return result;
 }
 
-void VocabularyMakeTests(){
+bool VocabularyMakeTests(){
 #ifdef VERBOSE
 	printf("[!] Starting Vocabulary Make Tests\n");
 #endif
@@ -21,36 +23,43 @@ void VocabularyMakeTests(){
 	struct lxc_vocabulary *first = LexicVocabularyAllocate(); ReportResult("Allocation", first != NULL);
 	
 	bool succ = LexicVocabularyDefinitionAdd(first, "digit", "[0-9]"); 
-	ReportResult("Add Def0", succ);
-	ReportResult("Add Def1", first->def_count == 1);
-	ReportResult("Add Def2", strcmp(first->definitions[0].name, "digit") == 0);
-	ReportResult("Add Def3", strcmp(first->definitions[0].regex, "[0-9]") == 0);
+	if (!succ) return false;
+	if (!ReportResult("Add Def1", succ)) return false;
+	if (!ReportResult("Add Def3", first->def_count == 1)) return false;
+	if (!ReportResult("Add Def3", strcmp(first->definitions[0].name, "digit") == 0)) return false;
+	if (!ReportResult("Add Def4", strcmp(first->definitions[0].regex, "[0-9]") == 0)) return false;
 
 	succ = LexicVocabularyDefinitionAdd(first, "abc", "abc");
-	ReportResult("Add Def4", succ);
-	ReportResult("Add Def5", first->def_count == 2);
+	if (!succ) return false;
+	if(!ReportResult("Add Def5", succ)) return false;
+	if(!ReportResult("Add Def6", first->def_count == 2)) return false;
 
 	LexicVocabularyFree(first);
 	
 	//Note that this will be built in _build folder/directory
 	first = LexicVocabularyFromFile("../example.vocab");
-	ReportResult("File1", first->def_count == 6);
-	ReportResult("File2", strcmp(first->definitions[0].name, "TYPE") == 0);
-	ReportResult("File3", strcmp(first->definitions[3].name, "NUMBER") == 0);
-	ReportResult("File4", strcmp(first->definitions[5].name, "SEMICOLON") == 0);
-	ReportResult("File5", strcmp(first->definitions[5].regex, ";") == 0);
-	ReportResult("File6", strcmp(first->definitions[2].regex, "=") == 0);
-	ReportResult("File7", strcmp(first->definitions[3].regex, "-?[0-9]+(\\.[0-9]*)?") == 0);
+	if (first == NULL) return false;
+	if (!ReportResult("File1", first->def_count == 6)) return false;
+	if (!ReportResult("File2", strcmp(first->definitions[0].name, "TYPE") == 0)) return false;
+	if (!ReportResult("File3", strcmp(first->definitions[3].name, "NUMBER") == 0)) return false;
+	if (!ReportResult("File4", strcmp(first->definitions[5].name, "SEMICOLON") == 0)) return false;
+	if (!ReportResult("File5", strcmp(first->definitions[5].regex, ";") == 0)) return false;
+	if (!ReportResult("File6", strcmp(first->definitions[2].regex, "=") == 0)) return false;
+	if (!ReportResult("File7", strcmp(first->definitions[3].regex, "-?[0-9]+\\.?[0-9]*") == 0)) return false;
+	//printf("file: '%s' vs input '-?[0-9]+\\.[0-9]*' -> strcmp = %d\n", first->definitions[3].regex, strcmp(first->definitions[3].regex, "-?[0-9]+\\.[0-9]*"));
 
 	LexicVocabularyFree(first);
+
 	first = LexicVocabularyFromString("THING: 00\n\n ITEM: 11");
-	ReportResult("String1", first->def_count == 2);
-	ReportResult("String2", strcmp(first->definitions[0].name, "THING") == 0);
-	ReportResult("String3", strcmp(first->definitions[1].regex, "11") == 0);
+	if (first == NULL) return false;
+	if (!ReportResult("String1", first->def_count == 2)) return false;
+	if (!ReportResult("String2", strcmp(first->definitions[0].name, "THING") == 0)) return false;
+	if (!ReportResult("String3", strcmp(first->definitions[1].regex, "11") == 0)) return false;
 
 #ifdef VERBOSE
 	printf("[!] Starting Vocabulary Make Tests\n");
 #endif
+	return true;
 }
 
 int main(void) {
