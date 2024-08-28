0259cef6f2ca0626986018fc560aad3668f3df57
HenriMalahieude <henrimalahieude@gmail.com>
2024-08-27 18:56:56 -0600

    beginnings of parsing unit

---
+++ Syntac/syntac.h
@@ -0,0 +1,51 @@
+#ifndef __HEPHAESTUS_SYNTACTICAL_ANALYZER__
+#define __HEPHAESTUS_SYNTACTICAL_ANALYZER__
+#include <stdbool.h>
+
+//In-Case someone decides to use Lexic (like me?)
+typedef struct lxc_token LexicToken;
+
+//Public facing container of grammatical rules
+typedef struct stc_book SyntacBook;
+
+typedef struct stc_tree_node {
+	struct stc_tree_node *children; //null-terminated
+	char *rule_name;
+	char *contains;
+	int line_start;
+	int col_start;
+	int line_end;
+	int col_end;
+} SyntacTreeNode;
+
+/*----------------------------Books----------------------------*/
+
+//Allocate and Populate a rule book from a file
+SyntacBook * SyntacBookFromFile(char *file_name);
+
+//Allocate and Populate a rule book from a string stream
+SyntacBook * SyntacBookFromString(char *stream);
+
+//Allocate an empty rule book
+SyntacBook * SyntacBookAllocate();
+
+//Add a new definition to a rule book (dynamic allocation of left and right)
+void SyntacBookRuleAdd(SyntacBook *book, char *left, char *right);
+
+//Free a rule book
+void SyntacBookFree(SyntacBook *);
+
+/*----------------------------Trees----------------------------*/
+
+//Generate a Syntactical Tree representation of tokens 
+//Expects token stream to be null-terminated
+SyntacTreeNode * SyntacTreeFromTokens(LexicToken *stream, SyntacBook *book);
+
+//Generate a Syntactical Tree representation of string stream
+//Expects token string stream to be null-terminated (stream[x] == NULL)
+SyntacTreeNode * SyntacTreeFromStream(char **stream, SyntacBook *book);
+
+//Free a null-terminated Syntactical Tree
+void SyntacTreeFree(SyntacTreeNode *);
+
+#endif
