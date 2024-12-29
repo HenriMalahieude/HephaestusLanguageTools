b66ac2544f6d57889d8b7692e17cf5089f66609d
HenriMalahieude <henrimalahieude@gmail.com>
2024-12-28 23:39:30 -0500

    added sets, need to test

---
+++ Syntac/private/sets.c
@@ -0,0 +1,61 @@
+#include <string.h>
+#include "sets.h"
+
+bool SetContains(char *item, char **set) {
+	if (set == NULL || item == NULL) return false;
+
+	int count = SetCount(set);
+	for (int i = 0; i < count; i++) {
+		char *elm = set[i];
+		if (strcmp(item, elm) == 0) return true;
+	}
+
+	return false;
+}
+
+//One by one add, may not be totally efficient
+//...
+//But honestly who cares, I got better things to do
+//NOTE: Could track capacity with special 0x1, 0x2, 0x0 "pointer" values (since first page still), but a little too much tbh
+bool SetAdd(char *item, char ***set) {
+	if (item == NULL || set == NULL) return false;
+	if (!SetContains(item, *set)) return false;
+
+	int count = SetCount(*set);
+	char **tmp = malloc(sizeof(char **) * (count+2));
+	if (tmp == NULL) return false; //malloc failed?
+	
+	for (int i = 0; i < count; i++) {
+		tmp[i] = (*set)[i]; //copy over the pointers to the strings
+	}
+	
+	tmp[count] = calloc(strlen(item)+1, sizeof(char));
+	strcpy(tmp[count], item);
+	tmp[count+1] = NULL;
+
+	if (*set != NULL) free(*set); //only free what has been jailed
+	*set = tmp;
+
+	return true;
+}
+
+int SetCount(char **set) {
+	if (set == NULL) return 0;
+
+	int cnt;
+	for (cnt = 0; set[cnt] != NULL; cnt++);
+
+	return cnt;
+}
+
+void SetFree(char **set) {
+	if (set == NULL) return;
+
+	int count = SetCount(set);
+	for (int i = 0; i < count; i++) {
+		free(set[i]);
+		set[i] = NULL;
+	}
+
+	free(set);
+}
---
+++ Syntac/private/sets.h
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+
+#ifndef __HEPH_SETS__
+#define __HEPH_SETS__
+
+//Sets of strings (unique, case-sensitive)
+//Sets must be NULL-Terminated
+
+//Set contains string, yes or no
+bool SetContains(char *item, char **set);
+
+//Outputs succesful (and unique) additions to the set
+//If it's in the set before, it returns false
+bool SetAdd(char *item, char ***set);
+
+//Amount of elements in set
+int SetCount(char **set);
+
+//Delete and reset set (free the memory too)
+void SetFree(char **set);
+
+#endif
